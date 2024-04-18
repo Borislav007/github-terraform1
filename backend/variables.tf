917fc0201aa40a050326d3e6b50b055c6f995880
Borislav007 <boris.miro@hotmail.com>
2024-04-18 11:11:58 +0200

    Første endring

---
+++ backend/variables.tf
@@ -0,0 +1,38 @@
+variable "rg_backend_name" {
+  type = string
+  description = "Name of the RG"
+}
+
+variable "rg_backend_location" {
+  type = string
+  description = "Name of the SA accesskey"
+}
+
+variable "sa_backend_name" {
+  type = string
+  description = "Name of the SA"
+}
+
+variable "sc_backend_name" {
+  type = string
+  description = "Name of the SA accesskey"
+}
+
+variable "kv_backend_name" {
+  type = string
+  description = "Name of the Key vauilt"
+}
+
+variable "sa_backend_accesskey_name" {
+  type = string
+  description = "Name of the SA accesskey"
+}
+
+variable "sc_backend" {
+  type = string
+  description = "Name of the SA container"
+}
+
+
+
+
